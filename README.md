ae00eee5247c650faff0e43af9856677e87a81d6
Maxime Dupuis <maxime.dupuis@logmein.com>
2021-12-01 16:33:13 -0500

    How to run

---
+++ README.md
@@ -0,0 +1,9 @@
+# Advent of code 2021!
+
+## How to run
+
+```
+erl
+c(advent),advent:day1().
+```
+
---
+++ advent.erl
@@ -0,0 +1,7 @@
+-module(advent).
+
+-export([day1/0]).
+
+day1() ->
+  io:fwrite("Hello, world!\n").
+
