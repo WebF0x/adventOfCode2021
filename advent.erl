af88a70827d083b2943115a69b6beb67ad6fce54
Sw1ftPanda <philippe.r.395@gmail.com>
2021-12-02 17:59:31 -0500

    Day2 Part 2! Ship it!

--- advent.erl
+++ advent.erl
@@ -85,6 +85,14 @@ day2_part1() ->
   ok.
 
 day2_part2() ->
+  {ok, Content} = file:read_file("input.txt"),
+  Lines = string:lexemes(Content, "\n"),
+  SplitLines = [string:lexemes(Line, " ") || Line <- Lines],
+  Commands = [{list_to_atom(binary_to_list(Command)), binary_to_integer(Distance)} || [Command, Distance] <- SplitLines],
+  Position = move_submarine_with_aim(Commands),
+  {X, Y} = Position,
+  io:fwrite("~p~n", [Position]),
+  io:fwrite("~p~n", [X * Y]),
   ok.
 
 move_submarine([Command | RemainingCommands]) ->
@@ -98,10 +106,29 @@ move_submarine([Command | RemainingCommands]) ->
 move_submarine([]) ->
   {0, 0}.
 
+
+move_submarine_with_aim(Commands) ->
+  move_submarine_with_aim(Commands, 0).
+move_submarine_with_aim([Command | RemainingCommands], Aim) ->
+  {DeltaX, DeltaY, NewAim} = case Command of
+    {up, Distance} -> {0, 0, Aim-Distance};
+    {forward, Distance} -> {Distance, Aim*Distance, Aim};
+    {down, Distance} -> {0, 0, Aim+Distance}
+  end,
+  {X, Y} = move_submarine_with_aim(RemainingCommands, NewAim),
+  {X + DeltaX, Y + DeltaY};
+move_submarine_with_aim([], _) ->
+  {0, 0}.
+
 day2_test() ->
   ?assertMatch({0, 0}, move_submarine([])),
   ?assertMatch({1, 0}, move_submarine([{forward, 1}])),
   ?assertMatch({0, -2}, move_submarine([{up, 2}])),
   ?assertMatch({0, 3}, move_submarine([{down, 3}])),
   ?assertMatch({15, 10}, move_submarine([{forward, 5}, {down, 5}, {forward, 8}, {up, 3}, {down, 8}, {forward, 2}])),
+
+  ?assertMatch({0, 0}, move_submarine_with_aim([])),
+  ?assertMatch({1, 0}, move_submarine_with_aim([{forward, 1}])),
+  ?assertMatch({2, 6}, move_submarine_with_aim([{down, 3}, {forward, 2}])),
+  ?assertMatch({15, 60}, move_submarine_with_aim([{forward, 5}, {down, 5}, {forward, 8}, {up, 3}, {down, 8}, {forward, 2}])),
   ok.
